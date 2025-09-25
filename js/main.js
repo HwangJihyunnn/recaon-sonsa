01795b9b26f3c15db8bffffb0d14fa3df048bb48
hjh <hjh_0524@naver.com>
2025-09-25 13:13:12 +0900

    250925첫화면

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const images = document.querySelectorAll('.main-img img');
+let current = 0;
+
+setInterval(() => {
+  images[current].classList.remove('active');
+  current = (current + 1) % images.length;
+  images[current].classList.add('active');
+}, 3000); // 4초 간격
