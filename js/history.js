60952294d2d26bc6a0bfefbe0e70f70d82fb8cc4
hjh <hjh_0524@naver.com>
2025-09-26 11:22:18 +0900

    intro수정중

---
+++ js/history.js
@@ -0,0 +1,30 @@
+async function loadHistory() {
+  try {
+    const response = await fetch("/js/history.json");
+    const data = await response.json();
+
+    const historyContainer = document.getElementById("history");
+    historyContainer.innerHTML = ""; // 초기화
+
+   data.forEach(item => {
+    const p = document.createElement("p");
+
+    const dateSpan = document.createElement("span");
+    dateSpan.classList.add("history-date"); // 날짜용 클래스
+    dateSpan.textContent = item.date;
+
+    const eventSpan = document.createElement("span");
+    eventSpan.classList.add("history-event"); // 이벤트용 클래스
+    eventSpan.textContent = item.event;
+
+    p.appendChild(dateSpan);
+    p.appendChild(eventSpan);
+
+    historyContainer.appendChild(p);
+    });
+  } catch (error) {
+    console.error("연혁 로딩 실패:", error);
+  }
+}
+
+loadHistory();
